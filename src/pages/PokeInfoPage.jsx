4314fd0eb59b46f5114d127a93bd630c4022a8fb
rep0land <eacosta3572@gmail.com>
2023-11-29 16:36:20 -0400

    second commit

--- src/pages/PokeInfoPage.jsx
+++ src/pages/PokeInfoPage.jsx
@@ -1,6 +1,6 @@
 import { useParams } from "react-router-dom"
-import useFetch from "../hooks/UseFetch"
 import { useEffect } from "react"
+import useFetch from "../hooks/useFetch"
 
 const PokeInfoPage = () => {
 
